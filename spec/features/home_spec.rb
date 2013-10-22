558e14f841175ceaec00afb32ba1d971767dbb51
Kelmer Perez <kelmerp@gmail.com>
2013-10-21 21:29:17 -0500

    added test looking for login link

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,5 +4,10 @@ describe 'home page' do
   it 'displays the home page' do
     visit '/'
     expect(page).to have_content('Fantasy Football Simplifier')
+  end  
+
+  it 'displays the the login link when not logged in' do
+    visit '/'
+    expect(page).to have_content('login for custom scoring')
   end
 end
